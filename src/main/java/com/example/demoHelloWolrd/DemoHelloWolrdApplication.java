b2936146d43295b6e43ecf342c767c065018d15e
Sudipta25 <sudiptachakraborty2506@gmail.com>
2022-04-12 17:01:29 +0530

    My first commit on Java Project

---
+++ src/main/java/com/example/demoHelloWolrd/DemoHelloWolrdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoHelloWolrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloWolrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloWolrdApplication.class, args);
+	}
+
+}
